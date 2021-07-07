93ed70b6988dfa653344af9fde7e728102ce0891
busainu <siodeitadaku@gmail.com>
2021-07-07 22:32:02 +0900

    add src

---
+++ src_manage/01_first_src_how_to_work_react/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+	// const para = document.createElement(`p`);
+	// para.textContent = `This is also visible`;
+	// document.getElementById(`root`).append(para);
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<p>This is also visible</p>
+		</div>
+	);
+}
+
+export default App;
