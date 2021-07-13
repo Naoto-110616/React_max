86cfcbd45d4df375a80315e1a7a32147d713e914
busainu <siodeitadaku@gmail.com>
2021-07-14 00:02:10 +0900

    add src

---
+++ src_manage/04_section/07_working_with_Multiple_state/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+	const [enteredTitle, setEnteredTitle] = useState(``);
+	const [enteredAmout, setEnteredAmout] = useState(``);
+	const [enteredDate, setEnteredDate] = useState(``);
+	const titleChangeHandoler = (event) => {
+		setEnteredTitle(event.target.value);
+	};
+	const amountChangeHandoler = (event) => {
+		setEnteredAmout(event.target.value);
+	};
+	const dateChangeHandoler = (event) => {
+		setEnteredDate(event.target.value);
+	};
+	return (
+		<form action="">
+			<div className="new-expense__controls">
+				<div className="new-expense__control">
+					<label>Title</label>
+					<input type="text" onChange={titleChangeHandoler} />
+				</div>
+				<div className="new-expense__control">
+					<label>Amount</label>
+					<input
+						type="text"
+						min="0.01"
+						step="0.01"
+						onChange={amountChangeHandoler}
+					/>
+				</div>
+				<div className="new-expense__control">
+					<label>Date</label>
+					<input
+						type="date"
+						min="2019-1-1"
+						step="2022-12-31"
+						onChange={dateChangeHandoler}
+					/>
+				</div>
+			</div>
+			<div className="new-expense__actions">
+				<button type="submit">Add Expense</button>
+			</div>
+		</form>
+	);
+};
+export default ExpenseForm;
