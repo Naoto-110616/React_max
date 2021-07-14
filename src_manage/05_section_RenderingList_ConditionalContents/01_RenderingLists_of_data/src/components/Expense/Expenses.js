e45ec5d3a16013476aa864873d4b1bb60db6871b
busainu <siodeitadaku@gmail.com>
2021-07-14 22:55:27 +0900

    add src

---
+++ src_manage/05_section_RenderingList_ConditionalContents/01_RenderingLists_of_data/src/components/Expense/Expenses.js
@@ -0,0 +1,31 @@
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
+
+const Expenses = (props) => {
+	const [filterYear, setFilteredYear] = useState(`2020`);
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+	return (
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter
+					selected={filterYear}
+					onChangeFilter={filterChangeHandler}
+				/>
+				{props.items.map((expense) => (
+					<ExpenseItem
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				))}
+			</Card>
+		</div>
+	);
+};
+
+export default Expenses;
