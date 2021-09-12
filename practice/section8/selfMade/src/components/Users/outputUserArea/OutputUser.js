9fd552ef339d4025c5e1ee15af3035ec78f0cc1c
Naoto-110616 <siodeitadaku@gmail.com>
2021-09-13 00:41:26 +0900

    add user info empty message

--- practice/section8/selfMade/src/components/Users/outputUserArea/OutputUser.js
+++ practice/section8/selfMade/src/components/Users/outputUserArea/OutputUser.js
@@ -3,19 +3,21 @@ import styles from "./OutputUser.module.css";
 import OutputUserList from "./OutputUserList";
 
 const OutputUser = (props) => {
+	let content = (
+		<p style={{ textAlign: "center" }}>No user info found. Maybe add one?</p>
+	);
+
+	if (props.userInfo.length > 0) {
+		content = props.userInfo.map((user) => (
+			<OutputUserList key={user.id} id={user.id} onDelete={props.deleteHandler}>
+				{user.name} ({user.age} years old)
+			</OutputUserList>
+		));
+	}
+
 	return (
 		<Card>
-			<ul className={`${styles[`outputArea`]}`}>
-				{props.userInfo.map((user) => (
-					<OutputUserList
-						key={user.id}
-						id={user.id}
-						onDelete={props.deleteHandler}
-					>
-						{user.name} ({user.age} years old)
-					</OutputUserList>
-				))}
-			</ul>
+			<ul className={`${styles[`outputArea`]}`}>{content}</ul>
 		</Card>
 	);
 };
