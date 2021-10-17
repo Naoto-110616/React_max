d03074268944d4339db86d305f798463d4607725
Naoto-110616 <siodeitadaku@gmail.com>
2021-10-17 23:39:57 +0900

    create header

--- self_made/01_starting_projects/src/components/Layout/Header.js
+++ self_made/01_starting_projects/src/components/Layout/Header.js
@@ -3,9 +3,9 @@ import classes from "./Header.module.css";
 
 import mealsImage from "../../assets/meals.jpg";
 
-const Header = (props) => {
+const Header = () => {
 	return (
-		<React.Fragment>
+		<>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
 				<button>Cart</button>
@@ -13,8 +13,7 @@ const Header = (props) => {
 			<div className={classes["main-image"]}>
 				<img src={mealsImage} alt="A table full of delicious food!" />
 			</div>
-		</React.Fragment>
+		</>
 	);
 };
-
 export default Header;
